76adce476882d05e2b21f61309ccdbbeb21c2b48
Lavansurendra <lavansurendra@gmail.com>
2026-04-25 00:49:43 -0500

    ran a couple tests with varied electron temperatures

--- scripts/Diffusion_eq_solver.py
+++ scripts/Diffusion_eq_solver.py
@@ -8,14 +8,14 @@ import csv
 # PARAMETERS — edit these freely
 # ─────────────────────────────────────────
 
-L = 1.02          # parallel connection length (chosen values: [1.02, 1.14, 1.33, 1.51, 3.33])
-Te = 5              # electron temperature in eV (chosen values: [5,6,7,8,9,10])
+L = 3.33          # parallel connection length (chosen values: [1.02, 1.14, 1.33, 1.51, 3.33])
+Te = 8              # electron temperature in eV (chosen values: [5,6,7,8,9,10])
 me = 9.11 * 10**(-31)    # electron mass in kg
 e = 1.602 * 10**(-19)   # elementary charge
 epsilon = 8.85 * 10**(-12)   # permittivity of free space (check units)
 N = 1000            # number of spatial grid points
 total_sim_time = 5 * 10**(-8)   # total time simulator runs (modify)
-n_steps = 150000   # total number of time steps to simulate (chosen values: {3.33: [], })
+n_steps = 37000   # total number of time steps to simulate (chosen values: {3.33: [12000, 19000, ], })
 animate_every = 1000  # only render every Nth frame (keeps animation smooth)
 
 n_baseline = 2 * 10**17      # baseline plasma density in m^(-3)
@@ -32,10 +32,10 @@ d_min_thres = (1/np.e) * bump_height + n_baseline   # disturbance height at cent
 recalculate_data = True
 
 # this variable should be set to the name of the csv you want to create or save to if the recalculate_data boolean is set to True, or the name of the csv you want to read from if the recalculate_data boolean is set to False
-csv_filename = "diffusion_results.csv"
+csv_filename = "dif_333_8.csv"
 
 # this variable should be set to the name of the gif you want to create corresponding to the animation
-gif_filename = "diffusion_animation.gif"
+gif_filename = "dif_333_8_animation.gif"
 
 # only recalculate data if the boolean is set to True, otherwise just read the data from the csv file and skip the simulation
 
